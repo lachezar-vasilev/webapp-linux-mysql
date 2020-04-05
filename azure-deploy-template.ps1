89492ee09b97cf6c6631b6196fa3d639dc395a81
lachezar-vasilev <rayedie@gmail.com>
2020-04-06 00:16:56 +0300

    add files

---
+++ azure-deploy-template.ps1
@@ -0,0 +1,126 @@
+
+#Connect-AzAccount
+#New-AzureRmResourceGroup RG-deploytest "West Europe"
+#$pw = Read-Host "Enter Pass for VM" -AsSecureString
+#New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateFile azure-testdeploy.json -TemplateParameterFile parameters.json -adminUsername admin123456 -adminPassword $pw
+
+
+
+Clear-Host
+
+###### LOGFILE
+$logfile = "c:\windows\logs\azure-template-deploy-"+(Get-Date -Format 'ddMMyyyy-HHmm')+".log"
+
+###### START CONSOLE RECORDING
+Start-Transcript -Path $logfile -Append
+
+###### SET EXECUTION POLICY 
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+
+###### AZURE TENANT SIGN-IN
+$Title = "Sign-in Tenant"
+Write-Host
+Write-Host
+Write-Host
+    Write-Host "    ================ $Title ================"
+    Write-Host
+    Write-Host "          [1]: MENTORMATE"
+    Write-Host "          [2]: ORTHOLOGY"
+	  Write-Host "          [3]: RAYEDIE"
+    Write-Host "                         Press 'Q' to quit."
+    Write-Host
+    Write-Host "    ========================================"
+	Write-Host
+	$selection = Read-Host "Please make a selection"
+	
+	switch ($selection)
+ {
+       '1' {
+         Connect-AzAccount -Tenant 3eaf366b-e9a9-4eca-b7ab-e5fa337c1fc3
+		   } 
+	   '2' {
+         Connect-AzAccount -Tenant 3a07883a-9a4f-4fa2-baa9-74333ce2926b
+		   }
+       '3' {
+         Connect-AzAccount -Tenant 8bb36e0c-62be-4c06-a562-733ed2b41f71
+           }
+       'q' {
+         return
+           }
+ }
+ 
+Write-Host
+Write-Host
+ 
+Get-AzResourceGroup
+ 
+Write-Host
+Write-Host
+
+Write-Host -NoNewLine 'Press any key to continue...' -ForegroundColor yellow
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+###### RESOURCE GROUP
+$rg = "rgr-test-" + (Get-Random -Maximum 100000)
+New-AzResourceGroup -Name $rg -Location centralus
+
+
+###### URL TEMPLATE
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-webapp-linux-managed-mysql/azuredeploy.json"
+$paramUri    = ""
+$templateUri1= "https://raw.githubusercontent.com/lachezar-vasilev/webapp-linux-mysql/master/2.test-template-vault.json"
+$paramUri1   = "https://raw.githubusercontent.com/lachezar-vasilev/webapp-linux-mysql/master/2.test-parameters-vault.json"
+
+
+###### DEPLOYMENT
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $templateUri -DeploymentDebugLogLevel All
+
+Write-Host -NoNewLine 'Press any key to continue...' -ForegroundColor yellow
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateUri $templateUri1 -TemplateParameterUri $paramUri1 -DeploymentDebugLogLevel All
+
+
+###### STOP CONSOLE RECORDING
+Stop-Transcript
+
+
+
+
+
+
+
+
+###### NOT USED
+#New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $param -DeploymentDebugLogLevel All
+#-TemplateParameterUri $paramUri
+#$template = "g:\My Drive\Azure\Code\azure-weblinux-mysql.json"
+#$param     = "g:\My Drive\Azure\Code\"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
